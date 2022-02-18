7dc5945d8d189fc7b099a3e41a854b117f0075fe
zhangyan <17746598041@163.com>
2022-02-18 11:12:19 +0800

    打包jar

--- build.sh
+++ build.sh
@@ -7,6 +7,5 @@ mvn package
 
 rm -rf dist
 mkdir dist
-cp target/mybatis-generater-1.0.0.jar dist/mybatis-generater-1.0.0.jar
-cp -r target/libs dist/libs
+cp target/mybatis-generater-1.0.0-jar-with-dependencies.jar dist/mybatis-generater-1.0.0-jar-with-dependencies.jar
 cp src/main/resources/config.properties dist/config.properties
